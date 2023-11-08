5e91d33456a8fc5c19e7f5f9aa7750fc5c7738ae
Dominic542 <130445757+Dominic542@users.noreply.github.com>
2023-11-08 18:36:22 +0200

    Update User.java

--- User.java
+++ User.java
@@ -1,52 +1,51 @@
-import java.util.ArrayList;
-import java.util.Random;
+public class HelloApplication {
+    private final int descriptiveVariable;
 
-public class Main {
-
-    private int nonDescriptiveVariableName;
-    private int uninitializedVariable;
-    private ArrayList<Integer> numberList = new ArrayList<>();
-
-    public Main(int x) {
-        nonDescriptiveVariableName = x;
+    public static void main(String[] args) {
+        HelloApplication mainObject = new HelloApplication(42);
+        int sum = mainObject.calculateSum(10, 20);
+        System.out.println("Sum: " + sum);
+        mainObject.complexLogic();
+        int factorialResult = mainObject.factorial(3);
+        System.out.println("Factorial: " + factorialResult);
+        mainObject.printNumbersAndCompleteLoop();
+        mainObject.calculateSumOfVariables(1, 2, 3, 5);
+    }
+    
+    public HelloApplication(int initialValue) {
+        descriptiveVariable = initialValue;
     }
 
-    public int getRandomValue(int n) {
-        if (numberList.size() == 0) {numberList.add(0);}
-        int random = new Random().nextInt(n);if (random != 0) return random; else return getRandomValue(n);}
-
-    public int    calculateSum(int a, int b) {return a+b;}
-
-    public void modifyList(int value) {
-        if (numberList.size() > 10) {
-            for     (int i = 0; i < numberList.size(); i++) {
-                int oldValue = numberList.get(i);
-                numberList.set(i, oldValue + value);
-            }
-        } else {
-            for (int i = 0; i < numberList.size(); i++)
-                numberList.set(i, value);
-        }
+    public int calculateSum(int a, int b) {
+        return a + b;
     }
 
     public int factorial(int n) {
-        if (n < 1) return 1;else return n * factorial(n-1);}
-
-    public void complexLogic() {if (uninitializedVariable == 0) throw new IllegalArgumentException("Uninitialized variable");}
-
-    public void overlyLongMethodWithLotsOfLines() {int i = 0; while (i < 100) {System.out.println(i); i++;}System.out.println("Loop complete.");}
-
-    public void methodWithExcessiveParameters(int a, int b, int c, int d, int e, int f, int g, int h, int i, int j, int k, int l) {
-        int result = a + b + c + d + e + f + g + h + i + j + k + l;
-        System.out.println("Result: " + result);
+        if (n < 1) {
+            return 1;
+        } else {
+            return n * factorial(n - 1);
+        }
     }
 
-    public static void main(String[] args) {
-        User messy = new User(42);
-        int sum = messy.calculateSum(10, 20);System.out.println("Sum: " + sum);
-        messy.complexLogic();
-        int fact = messy.factorial(5);System.out.println("Factorial: " + fact);
-        messy.overlyLongMethodWithLotsOfLines();
-        messy.methodWithExcessiveParameters(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12);
+    public void complexLogic() {
+        if (descriptiveVariable == 0) {
+            throw new IllegalArgumentException("Descriptive variable is uninitialized");
+        }
+    }
+
+    public void printNumbersAndCompleteLoop() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+        System.out.println("Loop complete.");
+    }
+
+    public void calculateSumOfVariables(int... numbers) {
+        int result = 0;
+        for (int num : numbers) {
+            result += num;
+        }
+        System.out.println("Result: " + result);
     }
 }
